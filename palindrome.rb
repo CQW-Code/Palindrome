56322923881bd52c5eb3983538ce8d5581290291
Chris Watkin <chriswatkin@gmail.com>
2018-02-18 16:53:33 -0700

    fixed compare code

--- palindrome.rb
+++ palindrome.rb
@@ -3,38 +3,45 @@
 #Welcome and ask for input
 
 def start()
-puts "Welcome to the Palindrome Palace!"
-puts "Pleace enter a word that is a palindrome"
-puts "enter q to Quit"
-user_input = gets.strip.downcase.split(" ")
-@word = []
-puts 
-puts "Your word is: #{user_input}"
-testword
-
-#put in an array to determine if palindrome... compare??
+  puts "Welcome to the Palindrome Palace!"
+	puts "Pleace enter a word that is a palindrome"
+	puts "enter q to Quit"
+	puts
+	user_input = gets.strip.downcase
+	
+	@word = []
+	testword(user_input)
+	
 end #ends start
+
+	#put in an array to determine if palindrome... compare
+def testword(user_input)
+	if user_input == 'q'
+    exit
+	end #end if
+	
+	puts "Your word is: #{user_input}"
+	puts
+
+  user_input.each_char do |letter|
+    @word << letter
+  end #end loop
+
+   @word.each_with_index do |letter, i|
+    if letter != @word[@word.length- (i +1)]
+		puts "#{user_input} isn't a palindrome, try again!"
+		puts
+    start
+	else
+		puts "Yay! A palindrome"
+		puts
+		start
+	end #end loop
+	end #end if
+end #end testword
 
 start
 
-
-def testword(user_input)
-    if user_input == 'q'
-        exit
-    end #end if
-    @user_input.each_char do |letter|
-       @word << letter
-  end #end loop
-
- @word.each_with_index do |letter , i|
-    if letter != @word.length-1
-    puts "That isn't a palindrome, try again!"
-    start
-    end #end if
-    puts "Yay! A palindrome"
-    start
-end
-end
 
 
 
